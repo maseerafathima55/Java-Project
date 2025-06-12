b7a343c2a625b2c73e8a618a33365bde536da55c
Maseera55 <shaikmaseerafathima@gmail.com>
2025-06-12 23:10:43 +0530

    my project notes

---
+++ frontend/src/App.js
@@ -0,0 +1,28 @@
+import EmployeeListComponent from "./components/EmployeeListComponent";
+import FooterComponent from "./components/FooterComponent";
+import HeaderComponent from "./components/HeaderComponent";
+import './App.css';
+import {BrowserRouter,Routes,Route} from 'react-router-dom';
+import CreateEmployeeComponent from "./components/CreateEmployeeComponent";
+
+function App()
+{
+  return (
+      <div className="bg-color">
+          <HeaderComponent/>
+
+          <BrowserRouter>
+             <div className="container mt-5">
+              <Routes>
+                  <Route exact path="/" element={<EmployeeListComponent/>}></Route>
+                  <Route path="/employees" element={<EmployeeListComponent/>}></Route>
+                  <Route path="/add-emp" element={<CreateEmployeeComponent/>}></Route>
+              </Routes>
+              </div> 
+          </BrowserRouter>
+          
+          <FooterComponent/>
+      </div>
+  )
+}
+export default App;
